5a8f73a59df1fb91bae098e67204b16f18ea3fb0
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-08 19:47:57 +0600

    Raw Folder added

--- 04.component_basic/src/components/Concept/concept.js
+++ 04.component_basic/src/components/Concept/concept.js
@@ -7,6 +7,7 @@ function Concept(props){
           <img src={props.image} alt={props.title} />
           <h2>{props.title}</h2>
           <p>{props.description}</p>
+          
         </li>
     )
 }
