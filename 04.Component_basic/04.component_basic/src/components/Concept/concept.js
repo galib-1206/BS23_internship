8854c5364932ef243df8516da3e80802bc141181
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-08 19:21:33 +0600

    First Commit

---
+++ 04.Component_basic/04.component_basic/src/components/Concept/concept.js
@@ -0,0 +1,14 @@
+
+// for removing code duplicacy, props is used as generalization instead of 3 concept class.
+function Concept(props){
+    return(
+
+<li className="concept">
+          <img src={props.image} alt={props.title} />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </li>
+    )
+}
+export default Concept;
+
